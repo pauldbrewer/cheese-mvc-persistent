18544cbad59966dce967369452571250cdf8e909
Paul Brewer <pauldbrewer@charter.net>
2017-04-11 07:11:15 -0500

    Completed assignment requirements.

---
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -0,0 +1,63 @@
+package org.launchcode.models.forms;
+
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Paul on 4/9/2017.
+ */
+public class AddMenuItemForm {
+
+
+    private int menuId;
+
+    private int cheeseId;
+
+    private Iterable<Cheese> cheeses;
+
+    private Menu menu;
+
+    public AddMenuItemForm() {
+    }
+
+    public AddMenuItemForm(Iterable<Cheese> cheeses, Menu menu) {
+        this.cheeses = cheeses;
+        this.menu = menu;
+    }
+
+    public int getMenuId() {
+        return this.menuId;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+    public Iterable<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void setCheeses(Iterable<Cheese> cheeses) {
+        this.cheeses = cheeses;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+}
+
+
---
+++ src/test/java/org/launchcode/controllers/MenuControllerTest.java
@@ -0,0 +1,10 @@
+package org.launchcode.controllers;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Paul on 4/9/2017.
+ */
+public class MenuControllerTest {
+
+}
